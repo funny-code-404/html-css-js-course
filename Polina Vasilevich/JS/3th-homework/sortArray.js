6fef7c98aea9b6f0ff84c57278b95bb5ae6059cb
Polina Vasilevich <polina.vasilevichh@gmail.com>
2020-11-21 21:59:55 +0300

    upgraded function sortArray

--- Polina Vasilevich/JS/3th-homework/sortArray.js
+++ Polina Vasilevich/JS/3th-homework/sortArray.js
@@ -1,22 +1,23 @@
 // 6. Написать функцию сортировки массива (дополнительно, определить параметром, в какую стороу сортировать)
 
+function swap(arr, j) {
+    temp = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = temp;
+}
+
 function sortArray(arr, isDesc = false) {
-    let temp = 0;
-    for(let i = 0; i <= arr.length - 1; i++) {
-        for(let j = 0; j <= arr.length - i - 1; j++) {
-            if(isDesc) {
-                if(arr[j] < arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            } else {
-                if(arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j <= arr.length - i; j++) {
+            const firstСondition = arr[j] > arr[j + 1];
+            const secondСondition = arr[j] < arr[j + 1];
+
+            if (!isDesc && firstСondition) {
+                swap(arr, j);
+            } else if (isDesc && secondСondition) {
+                swap(arr, j);
             }
+
         }
     }
 
@@ -25,3 +26,28 @@ function sortArray(arr, isDesc = false) {
 
 arr = [4, 2, 6, 8, 1, 3, 7, 5];
 console.log(sortArray(arr, 1));
+
+///////////////////////////////////////////////////
+///#2
+// function sortArray(arr, isDesc = false) {
+//     let temp = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+
+//     if (isDesc) {
+//         return arr.reverse();
+//     } else {
+//         return arr;
+//     }
+
+// }
+
+// arr = [4, 2, 6, 8, 1, 3, 7, 5];
+// console.log(sortArray(arr));
