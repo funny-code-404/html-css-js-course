f3aad12b69d260e2cf89c42f9860cb001015691d
Olya Smaliakova <ov.smaliakova@gmail.com>
2020-11-28 19:54:50 +0300

    изменения кода работы с объектами

--- function_js_4/returnObj.js
+++ function_js_4/returnObj.js
@@ -1,15 +1,22 @@
  /**Создать кэш данных в объекте и возвращать данные из
 кэша, если значение имеется или записывать в него
-новые значения, если не имеется. */
+новые значения, если не имеется. 
+
+Дополнить объект методом, который возвращает
+количество всех его ключей*/
 
 function AnyFunc () {
     const user = {
         name: "Tom",
         status: 0,
+        count: function CountObj(){
+             Object.key(this).length;
+        },
+
     }
-        return function returnObj (key) {
-            if (!user.hasOwnProperty(key)) {
-                user.key = 'new key';
+        return function returnObj (p, value) {
+            if (!user.hasOwnProperty(p)) {
+                user[p] = value;
                 }            
                     return user;
                 }
@@ -17,6 +24,8 @@ function AnyFunc () {
 
 const changeKey = AnyFunc();
 
-console.log(changeKey('name'));
-console.log(changeKey('status'));
-console.log(changeKey('just'));
+console.log(changeKey('name', "Tom"));
+console.log(changeKey('status', '0'));
+console.log(changeKey('just', 'just'));
+console.log(changeKey('just2', 'just2'));
+
