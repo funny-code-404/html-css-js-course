2a3647a67ec43eb52a13b912b2d64fdc990cee18
Olya Smaliakova <ov.smaliakova@gmail.com>
2020-12-15 22:06:54 +0300

    added js code + JSON

--- prakt_form/script/script.js
+++ prakt_form/script/script.js
@@ -3,6 +3,11 @@ const password = document.forms[0].password;
 const email = document.forms[0].email;
 const city = document.forms[0].city;
 const sendButton = document.getElementById('button-send');
+const checkbox1 = document.forms[1].check1;
+const checkbox2 = document.forms[1].check2;
+const checkbox3 = document.forms[1].check3;
+const anyComment = document.forms[1].comment;
+const resetButton = document.getElementById('button-reset');
 
 const handleClick = (e) => {
     e.preventDefault();
@@ -14,10 +19,30 @@ const handleClick = (e) => {
         name: nameUser.value,
         password: password.value,
         email: email.value,
-        city: city.value
+        city: city.value,
+        check1: checkbox1.checked,
+        check2: checkbox2.checked,
+        check3: checkbox3.checked,
+        comment: anyComment.value, 
     }
-    
+
+    const objectUser = JSON.stringify(user); 
+    //const parsedUser = JSON.parse(objectUser);
     console.log(user);
+    console.log(objectUser);
 }
 
-sendButton.addEventListener('click', handleClick)
+const handleClickReset = (e) => {
+    e.preventDefault();
+    nameUser.value = " ";
+    password.value = "";
+    email.value = " ";
+    city.value = " ";
+    checkbox1.checked = false;
+    checkbox2.checked = false;
+    checkbox3.checked = false;
+    anyComment.value = "";
+}
+
+sendButton.addEventListener('click', handleClick);
+resetButton.addEventListener('click', handleClickReset)
