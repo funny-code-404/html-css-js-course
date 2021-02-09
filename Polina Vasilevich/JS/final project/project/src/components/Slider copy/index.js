f028c4a540537a3330223b2b32e900c8c1c7defa
PolinaVasilevich <polina.vasilevichh@gmail.com>
2021-02-09 20:45:40 +0300

    added slider

--- Polina Vasilevich/JS/final project/project/src/components/Slider copy/index.js
+++ Polina Vasilevich/JS/final project/project/src/components/Slider copy/index.js
@@ -7,36 +7,36 @@ class Slider extends React.Component {
     super(props);
     this.id = props.id;
     this.state = {
-      lastIndex: 4,
-      firstIndex: 1,
+      position: 0,
+      firstIndex: 0,
+      lastIndex: 3,
     };
-
   }
-  
+
   nextSlider = (e) => {
     e.preventDefault();
-    const imgs = document.getElementById(this.id).getElementsByClassName('img1');
-  
-    
-
+    const imgs = document
+      .getElementById(this.id)
+      .getElementsByClassName("img1");
+    if (this.state.lastIndex < imgs.length - 1) {
+      const newPosition =
+        this.state.position === imgs.length - 1 ? 0 : this.state.position + 1;
+      this.setState((prevState) => {
+        return { position: newPosition, lastIndex: prevState.lastIndex + 1 };
+      });
+    }
   };
 
   prevSlider = (e) => {
     e.preventDefault();
-    const imgs = document.getElementById(this.id).getElementsByClassName('img1');
-    if (this.state.currentSliderIndex > 1) {
+    const imgs = document
+      .getElementById(this.id)
+      .getElementsByClassName("img1");
+    if (this.state.firstIndex > -1) {
+      const newPosition =
+        this.state.position === 0 ? imgs.length - 1 : this.state.position - 1;
       this.setState((prevState) => {
-        this.setOpacity(
-          imgs,
-          prevState.currentSliderIndex - 1,
-          prevState.currentSliderIndex - 2
-        );
-        return { currentSliderIndex: prevState.currentSliderIndex - 1 };
-      });
-    } else {
-      this.setState((prevState) => {
-        this.setOpacity(imgs, prevState.currentSliderIndex - 1, imgs.length - 1);
-        return { currentSliderIndex: imgs.length };
+        return { position: newPosition, firstIndex: prevState.firstIndex - 1 };
       });
     }
   };
@@ -54,47 +54,53 @@ class Slider extends React.Component {
   // };
 
   render() {
-    const { imgs} = this.props.items;
-    const {id, isDots, isArrows} = this.props;
+    const { imgs } = this.props.items;
+    const { id, isDots, isArrows } = this.props;
     return (
       <div id={id}>
         <div className="slides1">
           {imgs.map((img, index) => {
-            return <img key={index} className="img1" src={img} alt="img" style={{
-          transition: 'transform ease-out 450ms', 
-          transform: `translateX(-${this.state.position}00%)`
-        }}/>;
+            return (
+              <img
+                key={index}
+                className="img1"
+                src={img}
+                alt="img"
+                style={{
+                  transition: "transform ease-out 450ms",
+                  transform: `translateX(-${this.state.position}00%)`,
+                }}
+              />
+            );
           })}
 
           {isArrows && (
             <>
               <button className="prev" onClick={this.prevSlider}>
-              &#10094;
-            </button>
-            <button className="next" onClick={this.nextSlider}>
-              &#10095;
-            </button>
-          </>
+                &#10094;
+              </button>
+              <button className="next" onClick={this.nextSlider}>
+                &#10095;
+              </button>
+            </>
           )}
-          
-          
+
           {isDots && (
             <div className="slider-dots">
-          {imgs.map((img, index) => {
-            return (
-              <button
-                id={index}
-                className={`slider-dots_item ${
-                  index + 1 === this.state.currentSliderIndex && "active"
-                } `}
-                onClick={this.changeSlider}
-              ></button>
-            );
-          })}
-        </div>
+              {imgs.map((img, index) => {
+                return (
+                  <button
+                    id={index}
+                    className={`slider-dots_item ${
+                      index + 1 === this.state.currentSliderIndex && "active"
+                    } `}
+                    onClick={this.changeSlider}
+                  ></button>
+                );
+              })}
+            </div>
           )}
         </div>
-
       </div>
     );
   }
