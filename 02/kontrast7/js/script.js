1c6e6fa5b29dd3a30772f4b6088e7aca777d62de
kontrast7 <maks.mashko@gmail.com>
2021-04-14 11:57:33 +0300

    Update script.js

--- 02/kontrast7/js/script.js
+++ 02/kontrast7/js/script.js
@@ -1,89 +1,88 @@
-// // скопировать массив
-// const fruits = ['1', '2', '3'];
-// const newArr = fruits.slice(0);
-// console.log(newArr)
+// скопировать массив
+const fruits = ['1', '2', '3'];
+const newArr = fruits.slice(0);
+console.log(newArr)
 
 
-// // преобразовать массив в строку
-// let newStr = '';
-// for (let i = 0; i < fruits.length; i++) {
-//     newStr += fruits[i];
-// }
-// console.log(newStr);
+// преобразовать массив в строку
+let newStr = '';
+for (let i = 0; i < fruits.length; i++) {
+    newStr += fruits[i];
+}
+console.log(newStr);
 
 
-// // получить сумму элементов массива
-// const fruits1 = ['1', '2', '3'];
-// let newStr2 = 0;
-// for (let i = 0; i < fruits1.length; i++) {
-//     newStr2 += +fruits1[i];
-// }
-// console.log(newStr2);
+// получить сумму элементов массива
+const fruits1 = ['1', '2', '3'];
+let newStr2 = 0;
+for (let i = 0; i < fruits1.length; i++) {
+    newStr2 += +fruits1[i];
+}
+console.log(newStr2);
 
 
-// // удалить ложные значения из массива
-// const falsyArr = [NaN, 0, '', 15, -17];
-// // trueOnly = falsyArr.filter(Boolean);
-// // console.log(trueOnly);
-// let trueArr = [];
-// for (let i = 0; i < falsyArr.length; i++) {
-//     if (falsyArr[i]) {
-//         trueArr.push(falsyArr[i]);
+// удалить ложные значения из массива
+const falsyArr = [NaN, 0, '', 15, -17];
+// trueOnly = falsyArr.filter(Boolean);
+// console.log(trueOnly);
+for (let i = 0; i < falsyArr.length; i++) {
+    if (falsyArr[i] === "" || !falsyArr[i]) {
+        falsyArr.shift();
+    }
+}
+console.log(falsyArr);
+
+
+// удалить повторяющиеся элементы из массива
+const numArr = ['1', '1', '3', '1', '3', '3'];
+// let newNumArr = 0;
+// for (let i = 0; i < numArr.length; i++) {
+//     if (numArr[i] == numArr[i - 1]) {
+//         ++newNumArr;
 //     }
 // }
-// console.log(trueArr);
+// console.log(newNumArr);
+let arr_1 = Array.from(new Set(numArr));
+console.log(arr_1);
 
 
-// // удалить повторяющиеся элементы из массива
-// const numArr = ['1', '1', '3', '1', '3', '3'];
-// // let newNumArr = 0;
-// // for (let i = 0; i < numArr.length; i++) {
-// //     if (numArr[i] == numArr[i - 1]) {
-// //         ++newNumArr;
-// //     }
-// // }
-// // console.log(newNumArr);
-// let arr_1 = Array.from(new Set(numArr));
-// console.log(arr_1);
+// вывести число повторяющихся элементов
+const numArr2 = ['1', '1', '3', '1', '3', '3', '7', '7', '5', '5', '5'];
+let arr_2 = Array.from(new Set(numArr2));
+console.log(numArr2.length - arr_2.length);
 
 
-// // вывести число повторяющихся элементов
-// const numArr2 = ['1', '1', '3', '1', '3', '3', '7', '7', '5', '5', '5'];
-// let arr_2 = Array.from(new Set(numArr2));
-// console.log(numArr2.length - arr_2.length);
+// как тебя зовут
+const Name = prompt('Как тебя зовут?', '');
+
+function sayHello() {
+    alert(`Привет ${Name}`);
+}
+sayHello(Name);
 
 
-// // как тебя зовут
-// const Name = prompt('Как тебя зовут?', '');
+// min max
+let arr111 = [-1, 560, 10, 20, -20];
 
-// function sayHello() {
-//     alert(`Привет ${Name}`);
-// }
-// sayHello(Name);
+function minMax(arr) {
+    let min = 0;
+    let max = 0;
+    let arrrr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    arrrr[0] = min;
+    arrrr[1] = max;
+    return arrrr;
 
+}
 
-// // min max
-// let arr111 = [-1, 560, 10, 20, -20];
-
-// function minMax(arr) {
-//     let min = 0;
-//     let max = 0;
-//     let arrrr = []
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] < min) {
-//             min = arr[i];
-//         }
-//         if (arr[i] > max) {
-//             max = arr[i];
-//         }
-//     }
-//     arrrr[0] = min;
-//     arrrr[1] = max;
-//     return arrrr;
-
-// }
-
-// console.log(minMax(arr111));
+console.log(minMax(arr111));
 
 
 // калькулятор
