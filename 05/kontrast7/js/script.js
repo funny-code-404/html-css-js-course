11049372ad2c7e64b306bb8d3626685d23b57195
kontrast7 <maks.mashko@gmail.com>
2021-04-21 18:28:11 +0300

    Update script.js

--- 05/kontrast7/js/script.js
+++ 05/kontrast7/js/script.js
@@ -45,10 +45,19 @@ class Td {
         container.appendChild(this.td);
     }
 }
-const td1 = new Td('i1', '1', main);
-const td2 = new Td('i2', '2', main);
-const td3 = new Td('i3', '3', main);
-const td4 = new Td('i4', '4', main);
+
+
+function createTd(countTd, text, way) {
+    for (let i = 1; i <= countTd; i++) {
+        const t = new Td(`idTd${i}`, text, way);
+    }
+}
+createTd(4, "", main)
+
+// const td1 = new Td('i1', '1', main);
+// const td2 = new Td('i2', '2', main);
+// const td3 = new Td('i3', '3', main);
+// const td4 = new Td('i4', '4', main);
 
 
 class Tr {
@@ -63,23 +72,43 @@ class Tr {
         container.appendChild(this.tr);
     }
 }
-const tr1 = new Tr('id1', 'warning', i1);
-const tr2 = new Tr('id2', 'warning', i1);
-const tr3 = new Tr('id3', 'warning', i1);
-const tr4 = new Tr('id4', 'warning', i1);
 
 
-const tr5 = new Tr('id5', 'error', i2);
-const tr6 = new Tr('id6', 'error', i2);
-const tr7 = new Tr('id7', 'error', i2);
-const tr8 = new Tr('id8', 'error', i2);
+function createTr(countTd, text1 = 1, text2 = 2, text3 = 3, text4 = 4) {
+    for (let i = 1; i <= countTd; i++) {
+        const tr1 = new Tr(`idTr${i}`, text1, idTd1);
+    }
+    for (let j = 1; j <= countTd; j++) {
+        const tr2 = new Tr(`idTr${j}`, text2, idTd2);
+    }
+    for (let i = 1; i <= countTd; i++) {
+        const tr3 = new Tr(`idTr${i}`, text3, idTd3);
+    }
+    for (let i = 1; i <= countTd; i++) {
+        const tr4 = new Tr(`idTr${i}`, text4, idTd4);
+    }
+}
+createTr(4, '111', '222', '333', '444')
 
-const tr9 = new Tr('id9', 'primary', i3);
-const tr10 = new Tr('id10', 'primary', i3);
-const tr11 = new Tr('id11', 'primary', i3);
-const tr12 = new Tr('id12', 'primary', i3);
 
-const tr13 = new Tr('id13', 'default', i4);
-const tr14 = new Tr('id14', 'default', i4);
-const tr15 = new Tr('id15', 'default', i4);
-const tr16 = new Tr('id16', 'default', i4);
+
+// const tr1 = new Tr('id1', 'warning', idTd1);
+// const tr2 = new Tr('id2', 'warning', idTd1);
+// const tr3 = new Tr('id3', 'warning', idTd1);
+// const tr4 = new Tr('id4', 'warning', idTd1);
+
+
+// const tr5 = new Tr('id5', 'error', idTd2);
+// const tr6 = new Tr('id6', 'error', idTd2);
+// const tr7 = new Tr('id7', 'error', idTd2);
+// const tr8 = new Tr('id8', 'error', idTd2);
+
+// const tr9 = new Tr('id9', 'primary', idTd3);
+// const tr10 = new Tr('id10', 'primary', idTd3);
+// const tr11 = new Tr('id11', 'primary', idTd3);
+// const tr12 = new Tr('id12', 'primary', idTd3);
+
+// const tr13 = new Tr('id13', 'default', idTd4);
+// const tr14 = new Tr('id14', 'default', idTd4);
+// const tr15 = new Tr('id15', 'default', idTd4);
+// const tr16 = new Tr('id16', 'default', idTd4);
