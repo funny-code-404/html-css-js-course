79225869946da3afc0ac300da136486dfe26de40
kontrast7 <maks.mashko@gmail.com>
2021-04-22 13:23:04 +0300

    add Style in JS

--- 05/kontrast7/js/script.js
+++ 05/kontrast7/js/script.js
@@ -34,27 +34,41 @@ showNotification('123')
 
 // TABLE_______________________________________________
 class Td {
-    constructor(id, text, container = document.body) {
+    constructor(id, text, container = document.body, status) {
         this.td = document.createElement('td');
         this.td.id = id;
         this.td.innerHTML = text;
         this.render(container);
+        this.status = status;
+        this.setStyles();
     }
 
     render(container) {
         container.appendChild(this.td);
     }
+
+    setStyles() {
+        switch (this.status) {
+            case 'primary':
+                return this.td.classList.add('primary');
+            case 'warning':
+                return this.td.classList.add('warning');
+            case 'error':
+                return this.td.classList.add('error');
+            default:
+                return this.td.classList.add('default');
+        }
+    }
 }
 
-function createTd(countTd, text, way) {
+function createTd(countTd, text, way, status = 'default') {
     for (let i = 1; i <= countTd; i++) {
-        const t = new Td(`idTd${i}`, text, way);
+        const t = new Td(`idTd${i}`, text, way, status);
     }
     return countTd;
 }
 
-const tdCount = createTd(6, "1234", main);
-
+const tdCount = createTd(6, "1234", main, 'error');
 
 
 // const td1 = new Td('i1', '1', main);
@@ -64,28 +78,39 @@ const tdCount = createTd(6, "1234", main);
 
 
 
-
-
 class Tr {
-    constructor(id, text, container = document.body) {
+    constructor(id, text, container = document.body, status) {
         this.tr = document.createElement('tr');
         this.tr.id = id;
         this.tr.innerHTML = text;
         this.render(container);
+        this.status = status;
+        this.setStyles();
     }
-
     render(container) {
         container.appendChild(this.tr);
+    }
+    setStyles() {
+        switch (this.status) {
+            case 'primary':
+                return this.tr.classList.add('primary');
+            case 'warning':
+                return this.tr.classList.add('warning');
+            case 'error':
+                return this.tr.classList.add('error');
+            default:
+                return this.tr.classList.add('default');
+        }
     }
 }
 
 
-function createTr(countTr, text = '') {
+function createTr(countTr, text = '', status = 'default') {
     for (let i = 1; i <= tdCount; i++) {
         for (let j = 1; j <= countTr; j++) {
             let string = `idTd${i}`;
             let object = eval("(" + string + ")");
-            const tr = new Tr(`idTr${i}`, text, object);
+            const tr = new Tr(`idTr${i}`, text, object, status);
 
         };
     };
