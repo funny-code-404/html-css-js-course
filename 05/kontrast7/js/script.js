58e935cd7f33bb86991d100d6bb1c4361b9bf911
kontrast7 <maks.mashko@gmail.com>
2021-04-25 10:45:36 +0300

    Update script.js

--- 05/kontrast7/js/script.js
+++ 05/kontrast7/js/script.js
@@ -11,7 +11,7 @@ li3.style.color = 'red';
 li4.style.color = 'red';
 
 
-// решение массивом
+// решение циклом
 for (let i = 0; i < ul.children.length; i++) {
     ul.children[i].firstElementChild.style.color = 'red';
 }
@@ -71,13 +71,6 @@ function createTd(countTd, text, way, status = 'default') {
 const tdCount = createTd(6, "1234", main, 'error');
 
 
-// const td1 = new Td('i1', '1', main);
-// const td2 = new Td('i2', '2', main);
-// const td3 = new Td('i3', '3', main);
-// const td4 = new Td('i4', '4', main);
-
-
-
 class Tr {
     constructor(id, text, container = document.body, status) {
         this.tr = document.createElement('tr');
@@ -114,41 +107,5 @@ function createTr(countTr, text = '', status = 'default') {
 
         };
     };
-
-    // for (let i = 1; i <= countTr; i++) {
-    //     const tr1 = new Tr(`idTr${i}`, text, idTd1);
-    // };
-    // for (let j = 1; j <= countTr; j++) {
-    //     const tr2 = new Tr(`idTr${j}`, text, idTd2);
-    // };
-    // for (let i = 1; i <= countTr; i++) {
-    //     const tr3 = new Tr(`idTr${i}`, text, idTd3);
-    // };
-    // for (let i = 1; i <= countTr; i++) {
-    //     const tr4 = new Tr(`idTr${i}`, text, idTd4);
-    // };
 }
 createTr(6, 'ячейка');
-
-
-
-// const tr1 = new Tr('id1', 'warning', idTd1);
-// const tr2 = new Tr('id2', 'warning', idTd1);
-// const tr3 = new Tr('id3', 'warning', idTd1);
-// const tr4 = new Tr('id4', 'warning', idTd1);
-
-
-// const tr5 = new Tr('id5', 'error', idTd2);
-// const tr6 = new Tr('id6', 'error', idTd2);
-// const tr7 = new Tr('id7', 'error', idTd2);
-// const tr8 = new Tr('id8', 'error', idTd2);
-
-// const tr9 = new Tr('id9', 'primary', idTd3);
-// const tr10 = new Tr('id10', 'primary', idTd3);
-// const tr11 = new Tr('id11', 'primary', idTd3);
-// const tr12 = new Tr('id12', 'primary', idTd3);
-
-// const tr13 = new Tr('id13', 'default', idTd4);
-// const tr14 = new Tr('id14', 'default', idTd4);
-// const tr15 = new Tr('id15', 'default', idTd4);
-// const tr16 = new Tr('id16', 'default', idTd4);
