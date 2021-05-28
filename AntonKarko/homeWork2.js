e6ef0af019a2446fcc10ab30ace7732e68dcb475
KOSHAK2008 <81808174+KOSHAK2008@users.noreply.github.com>
2021-05-28 18:17:23 +0300

    Update homeWork2.js

--- AntonKarko/homeWork2.js
+++ AntonKarko/homeWork2.js
@@ -1,5 +1,5 @@
 // Создайте функцию клькулятора
-/*function calcul () {
+function calcul () {
     let num1;
     let num2;
     let act;
@@ -24,20 +24,20 @@
             console.log(num1 ** num2);
         }
 };
-calcul();*/
+calcul();
 
 // Напишите функцию замыкания, которая хранит и складывает принимаемые аргументы в сумму и возвращает ее.
-/*function summ() {
+function summ() {
     return function (a, b) {
         return a + b;      
     };
 }
 let suma = summ ();
-suma(1,5);*/
+suma(1,5);
 
 // Создать кэш данных в обьекте и возвращать данные из кэша, если значение имеется или записыватьт в него новые значения, если не имеется.
 
-/*let obj = {
+let obj = {
     name: "Anton",
     age: 33,
     status,
@@ -48,7 +48,8 @@ suma(1,5);*/
         //         obj[value] = 'ok'
         //     }
         // }console.log(value);
-        for (let [key, value] in Object.entries(obj)) {
+        // Это не сделано еще
+for (let [key, value] in Object.entries(obj)) {
             if (this.obj.hasOwnProperty() == false) {
                 this.obj[key, value] = this.obj[key, 'ok']
             };
@@ -61,11 +62,11 @@ suma(1,5);*/
 };
 console.log(obj);
 
-obj.NumberOfProperties();*/
+obj.NumberOfProperties();
 
 // Создать обьект машины car, который имеет свойство "передача" и метод, который повышает/понижает передачу на 1. Также у обьекта должен быть метод, который в зависимости от передачи возвращает сообщение о скорости: (-1) - 'back', 0 - 'neutral', 1 - 'slow', 2 - 'medium', 3 - 'fast', 4- 'very fast'. При попытке сложить обьект car с числом - число должно складывать в свойство speed и speed должен возвращатьсяю Если скорость больше 5 - должно быть выведено сообщение "Warning! Your speed is too high!"
 
-/*const car = {
+const car = {
    transmission: 0,
     upTransmission: function () { this.transmission += 1;},
     downTransmission: function () { this.transmission -= 1;},
@@ -91,4 +92,4 @@ obj.NumberOfProperties();*/
 
 car.downTransmission();
 car.upTransmission();
-car.shownTransmission();*/
+car.shownTransmission();
