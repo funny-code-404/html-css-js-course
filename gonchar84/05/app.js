72f3c0cc70ec835646f04fa6549bb3e48115982f
Lena <gonhar84@tut.by>
2021-06-14 17:10:58 +0300

    dom

---
+++ gonchar84/05/app.js
@@ -0,0 +1,33 @@
+// Получить:
+// - тэг с Users
+// - тэг с Sam
+// - тэг Jane
+
+const users = document.body.firstElementChild;
+
+const sam = document.body.firstElementChild.nextElementSibling.lastElementChild;
+
+const jane = document.body.firstElementChild.nextElementSibling.firstElementChild;
+console.log(users, sam, jane);
+// Задание
+// - сверстать данный
+// элемент
+// - задать выделенным
+// элементам цвет фона
+// (любой)
+// elem.style.backgroundColor = 'red';
+const element = document.body.lastElementChild.previousElementSibling.previousElementSibling.lastElementChild.style.backgroundColor = 'red';
+console.log(element)
+
+// Задание
+// - сверстать данный
+// элемент
+// - задать выделенным
+// элементам цвет фона
+// (любой)
+
+let table = document.body.lastElementChild.previousElementSibling.firstElementChild.style.backgroundColor = 'red';
+let table2 = document.body.lastElementChild.previousElementSibling.lastElementChild.style.backgroundColor = 'red';
+console.log(table2)
+let table3 = five.style.backgroundColor = 'red';
+console.log(table3)
