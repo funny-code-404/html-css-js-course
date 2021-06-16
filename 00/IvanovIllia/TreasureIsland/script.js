47d57fd01bda858f4659c503e4d2be52e3c49f3b
IvanovIllia <ravenolog@rambler.ru>
2021-06-16 17:16:14 +0300

    why dosn't work?

---
+++ 00/IvanovIllia/TreasureIsland/script.js
@@ -0,0 +1,27 @@
+let succsesClick = function () {
+    alert('Нашёл');
+};
+
+
+mapMark.onclick = succsesClick;
+
+TrMap.onclick = function() {
+    alert('Мимо! Попробуй ещё!');
+};
+
+
+
+
+
+
+
+// let TM = document.createElement ('div');
+
+// TM.id = TrMap;
+
+// let MM = document.createElement ('div');
+
+// MM.id = mapMark;
+
+// TM.appendChild(MM);
+
