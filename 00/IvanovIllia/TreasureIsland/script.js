17e6e53dcb6be9c4d263f84b3e66ff6dff56a806
IvanovIllia <ravenolog@rambler.ru>
2021-06-17 08:21:20 +0300

    some changes, but don't work anyway

--- 00/IvanovIllia/TreasureIsland/script.js
+++ 00/IvanovIllia/TreasureIsland/script.js
@@ -1,3 +1,19 @@
+//Прописываем в DOM
+
+let TM = document.createElement ('div');
+
+TM.id = TrMap;
+
+let MM = document.createElement ('div');
+
+MM.id = mapMark;
+
+TM.appendChild(MM);
+
+document.body.appendChild(TM);
+
+
+//Пишем события
 let a = document.getElementById('mapMark');
 a.onclick = function () {
     alert('Нашёл');
@@ -14,14 +30,4 @@ b.onclick = function() {
 
 
 
-
-// let TM = document.createElement ('div');
-
-// TM.id = TrMap;
-
-// let MM = document.createElement ('div');
-
-// MM.id = mapMark;
-
-// TM.appendChild(MM);
 
