1cdd00e9db4dbdbfab9e5bff5096b6f8c001f440
IvanovIllia <ravenolog@rambler.ru>
2021-07-03 18:01:16 +0300

    to do list

---
+++ 00/IvanovIllia/ToDo/script.js
@@ -0,0 +1,43 @@
+function checkSpace(){
+    let textSpace = document.getElementById("task").value;
+    if (textSpace == ''){ 
+        alert('Enter the task');
+    }else {
+        const textSpace = document.getElementById("task").value;
+        const taskList = document.createElement('li');
+        const text = document.createTextNode(textSpace);
+        ol.appendChild(taskList)
+        taskList.appendChild(text);
+        let checkButton = document.createElement('button');
+        checkButton.innerHTML = 'In Progress';
+        checkButton.classList.add('inProgress');
+        taskList.appendChild(checkButton);
+        document.getElementById("task").value = '';
+        markList(checkButton)
+        }
+}
+
+
+
+
+
+const checkText = document.querySelector("body > main > div > button"); 
+
+checkText.addEventListener('click', checkSpace)
+
+function markList(checkButton){
+    checkButton.addEventListener('click', (event) => {
+        let taskStatus = checkButton.classList;
+        if (taskStatus == 'inProgress') {
+            checkButton.innerHTML = 'Done'
+            checkButton.classList.add('done');
+        }
+        else {
+            checkButton.innerHTML = 'In Progress'
+            checkButton.classList.add('inProgress');
+        }
+    })
+}
+
+
+    
