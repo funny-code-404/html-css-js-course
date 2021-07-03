652e5283f632ff6a03b4310fb450fda47964e57f
a.karko <antonkarko33@gmail.com>
2021-07-03 12:23:11 +0300

    HomeWork0307

---
+++ AntonKarko/HomeWorkwebPack/portfolio.js
@@ -0,0 +1,16 @@
+function renderPortfolio() {
+  const content = document.getElementById('content');
+  content.innerHTML = '';
+  let element = document.createElement("div");
+  element.id = 'portfolio'; 
+  element.classList.add('portfolio');
+  content.appendChild(element);
+  const spanText = document.createElement('span');
+  spanText.innerHTML = '<h1>О себе...</h1>';
+  content.appendChild(spanText);
+  spanText.classList.add('spanText');
+  const text = document.createElement('p');
+  text.classList.add('p');
+  spanText.appendChild(text);
+  text.innerHTML = '<h2>33 Года.</br> Работаю на жд. по професии "осмотрщик-ремонтник вагонов"</br> окончил Пту, ГГКЖТ, БелГУТ. (Магистр технических наук)</br> Решил сменить професиию на IT Front end</br> (так как нет дальнейшего роста и развития в нынешней професии)</br> </h2>'
+};
